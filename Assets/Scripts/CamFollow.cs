bd4cb13d83b6301d4973cfd34f59f0c323fa411f
emrekrts <emre28112004@gmail.com>
2021-08-04 22:08:38 +0300

    -test

---
+++ Assets/Scripts/CamFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public Transform karakter;
+    public float x = 5.5f;
+
+    void Start()
+    {
+        karakter = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    
+    void Update()
+    {
+        transform.position = new Vector3(karakter.position.x+x, 0, -10);
+    }
+}
---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    Rigidbody2D rgb;
+    Vector3 velocity;
+
+    float speedAmount = 5f;
+    float jumpAmount = 8f;
+
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0f);
+        transform.position += velocity * speedAmount * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && Mathf.Approximately(rgb.velocity.y,0))
+        {
+            rgb.AddForce(Vector3.up * jumpAmount, ForceMode2D.Impulse);
+        }
+    }
+}
